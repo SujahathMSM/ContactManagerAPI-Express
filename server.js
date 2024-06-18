6209b0b35dcbf6156edcd89372378968e216a3f5
sujahathmsm <sujahathmhmd@gmail.com>
2024-06-19 00:16:21 +0530

    Reversed to Old version

--- server.js
+++ server.js
@@ -1,17 +1,14 @@
-const express = require('express');
-const dotenv = require('dotenv').config();
+const express = require("express");
+const dotenv = require("dotenv").config();
 const PORT = process.env.PORT || 5000;
-const contactRouter = require('./routes/contactRoute');
-const errorHandler = require('./middleware/errorHandler');
-
+const contactRouter = require("./routes/contactRoute");
+const errorHandler = require("./middleware/errorHandler");
 
 app = express();
 // Middleware to Parse JSON data
-app.use(express.json())
-app.use(errorHandler)
+app.use(express.json());
+app.use(errorHandler);
 
-app.listen(PORT, () => 
-    console.log(`Server running on port ${PORT}`)
-);
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-app.use('/api/contacts', contactRouter)
+app.use("/api/contacts", contactRouter);
