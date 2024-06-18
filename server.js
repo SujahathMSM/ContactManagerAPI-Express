449432502e99d617d6a41d4e2175fe71bf4e2b31
sujahathmsm <sujahathmhmd@gmail.com>
2024-06-18 23:59:01 +0530

    Basic Routing

--- server.js
+++ server.js
@@ -1,2 +1,17 @@
-console.log("I am Sujahath");
-console.log("Welcomr to NODE JS");
+const express = require('express');
+const dotenv = require('dotenv').config();
+const PORT = process.env.PORT || 5000;
+const contactRouter = require('./routes/contactRoute');
+const errorHandler = require('./middleware/errorHandler');
+
+
+app = express();
+// Middleware to Parse JSON data
+app.use(express.json())
+app.use(errorHandler)
+
+app.listen(PORT, () => 
+    console.log(`Server running on port ${PORT}`)
+);
+
+app.use('/api/contacts', contactRouter)
